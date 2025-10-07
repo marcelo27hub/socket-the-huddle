d01a11f5957a50a373b01ad81676d14dc6887ccd
Marcelo Valdez <marcelogvz27@gmail.com>
2025-10-07 00:58:14 -0300

    ip y puerto

--- servidor.py
+++ servidor.py
@@ -1,2 +1,9 @@
 import socket
+import select
+"""IP	La dirección de la casa
+Puerto	La puerta específica de la casa
+Cliente	Invitado que llama a la puerta
+Servidor	Persona que abre la puerta y recibe al invitado"""
 
+host = '0.0.0.0' #escucha todas las interfaces/ ej : calle de mi casa
+port = 5000  # un canal de comunicación específico dentro de una computadora.
